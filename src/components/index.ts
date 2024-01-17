5535f80be025a7a41abb2ca4fa0a34a330e72230
UrijHoruzij <63039080+UrijHoruzij@users.noreply.github.com>
2024-01-17 17:07:07 +0000

    Deploying to deploy from  @ 92dadc2079e0c362a30d9e306533366ad1050b9c 🚀

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as SliderEvents } from './SliderEvents';
-export { default as Slider } from './Slider';
-export { default as Dot } from './Dot';
-export { default as Navigation } from './Navigation';
